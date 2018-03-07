939172c6960891526374a65a53091408e47f8286
Dan <dpedersen84@gmail.com>
2018-03-06 18:41:51 -0600

    added variables and newGame function

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+$(document).ready(function() { //START
+
+    //VARIABLES
+    let goalNumber;
+    let yourNumber;
+    let wins;
+    let losses;
+    let crystal1;
+    let crystal2;
+    let crystal3;
+    let crystal4;
+
+    //RESET FUNCTION
+
+    function newGame () {
+        let goalNumber;
+        let yourNumber;
+        let wins;
+        let losses;
+        let crystal1;
+        let crystal2;
+        let crystal3;
+        let crystal4;
+
+        $("#goalNumber, #yourNumber, #wins, #losses, #crystal1, #crystal2, #crystal3, #crystal4").empty();
+    }
+    //CLICK LISTENER
+
+}); //END
