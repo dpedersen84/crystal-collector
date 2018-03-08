a562dc3e48028b1561af5cff51c41352157fc5cf
Dan <dpedersen84@gmail.com>
2018-03-07 21:31:09 -0600

    all random numbers generated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,17 +23,20 @@ $(document).ready(function() { //START JQUERY
         let crystal3;
         let crystal4;
 
-        crystal1 = Math.floor((Math.random() * 19) + 1);
+        crystal1 = Math.floor((Math.random() * 12) + 1);
         console.log("Crystal 1: " + crystal1);
 
-        crystal2 = Math.floor((Math.random() * 19) + 1);
+        crystal2 = Math.floor((Math.random() * 12) + 1);
         console.log("Crystal 1: " + crystal2);
 
-        crystal3 = Math.floor((Math.random() * 19) + 1);
+        crystal3 = Math.floor((Math.random() * 12) + 1);
         console.log("Crystal 1: " + crystal3);
 
-        crystal4 = Math.floor((Math.random() * 19) + 1);
+        crystal4 = Math.floor((Math.random() * 12) + 1);
         console.log("Crystal 1: " + crystal4);
+
+        goalNumber = Math.floor((Math.random() * 120) + 19);
+        console.log("Goal Number: " + goalNumber);
 
         // $("#goalNumber, #yourNumber, #wins, #losses, #crystal1, #crystal2, #crystal3, #crystal4").empty(); //Clear Variables on DOM for New Game
 
