9a7378c8fb10f3c96bc68994669884d70bbcc9e1
Dan <dpedersen84@gmail.com>
2018-03-08 19:23:25 -0600

    crystal on click added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,7 @@ $(document).ready(function() { //START JQUERY
 
     function newGame () {
         let goalNumber;
-        let yourNumber;
+        let yourNumber=0;
         let wins;
         let losses;
         let crystal1;
@@ -23,7 +23,7 @@ $(document).ready(function() { //START JQUERY
         let crystal3;
         let crystal4;
 
-        crystal1 = Math.floor((Math.random() * 12) + 1);
+        crystal1 = parseInt((Math.random() * 12) + 1);
         console.log("Crystal 1: " + crystal1);
 
         crystal2 = Math.floor((Math.random() * 12) + 1);
@@ -37,20 +37,43 @@ $(document).ready(function() { //START JQUERY
 
         goalNumber = Math.floor((Math.random() * 120) + 19);
         console.log("Goal Number: " + goalNumber);
+        $("#goalNumber").text(goalNumber);
 
-        // $("#goalNumber, #yourNumber, #wins, #losses, #crystal1, #crystal2, #crystal3, #crystal4").empty(); //Clear Variables on DOM for New Game
+        // $("#yourNumber, #wins, #losses").empty(); //Clear Variables on DOM for New Game
 
     } //END FUNCTION
 
     newGame() ;
 
     //CLICK LISTENER
+    
+    //Crystal One
+    $(".crystal1").on("click", function () {
+        yourNumber += crystal1;
+        console.log("Your Number: " + yourNumber);
+        $("#yourNumber").text(yourNumber);
+    });
 
-    $(".crystal").on("click", function () {
-        
-        //IF ELSE
+    //Crystal Two
+    $(".crystal2").on("click", function () {
+        yourNumber += crystal2;
+        console.log("Your Number: " + yourNumber);
+        $("#yourNumber").text(yourNumber);
+    });
 
-    }) //END CLICK LISTENER
+    //Crystal Three
+    $(".crystal3").on("click", function () {
+        yourNumber += crystal3;
+        console.log("Your Number: " + yourNumber);
+        $("#yourNumber").text(yourNumber);
+    });
+
+    //Crystal Four
+    $(".crystal4").on("click", function () {
+        yourNumber += crystal4;
+        console.log("Your Number: " + yourNumber);
+        $("#yourNumber").text(yourNumber);
+    });
 
 
 }); //END JQUERY
